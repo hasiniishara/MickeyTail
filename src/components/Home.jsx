a89f5973a2168ff4109478ee47d5ed456088f4cd
HasiniIshara <hasiniisharao4@gmail.com>
2024-03-19 17:55:45 +0200

    Implementing MicketTail

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 export const Home = () => {
+
+  const categories = [
+    {
+      id:1,
+      name: "Meals"
+    },
+    {
+      id:2,
+      name: "Cocktails"
+    }
+  ]
+  
   return (
     <div>
       <div className='search-bar'>
