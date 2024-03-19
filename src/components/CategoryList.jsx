787077e21935a94f6b521f8016c43f25b85b7efb
HasiniIshara <hasiniisharao4@gmail.com>
2024-03-19 20:38:59 +0200

    Implementing MicketTail

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
-export const CategoryList = () => {
+export const CategoryList = ({categories, onSelectCategory}) => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="categories-row">
+          {
+            categories.map((item)=> (
+              <div className='category' key={item.id} onClick={()=>onSelectCategory(item.name)}>
+                  <p>{item.name}</p>
+              </div>
+            ))
+          }
+        </div>
+    </>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CategoryList } from './CategoryList'
 
 export const Home = () => {
@@ -14,13 +14,27 @@ export const Home = () => {
     }
   ]
 
+  const [selectedCategoryName, setSelectedCategoryName] = useState('Meals');
+
+  const onSelectCategory = (clickCategoryName) =>{
+
+    setSelectedCategoryName(clickCategoryName);
+
+  }
+
   return (
-    <div>
+  <>
       <div className='search-bar'>
         <input type="text" id="search" placeholder="Search..."/>
         <button onclick="search()">Search</button>
+      </div>   
+      <div className="home">
+          <CategoryList 
+            categories = {categories}
+            onSelectCategory= {onSelectCategory}
+          />
       </div>
-    </div>
+    </>
   )
 }
 
